9a03fed07ac1779dcbc9d5beef5148acf6e11e1d
Sufyan Ahmed <muhammadsufyan20@gmail.com>
2025-02-15 00:28:33 +0500

    Add gitignore and added system variables

---
+++ agent.py
@@ -0,0 +1,6 @@
+import os
+
+GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
+OPEN_AI_KEY = os.environ.get('DEEP_SEEK_API_KEY')
+TAVILY_API_KEY = os.environ.get('TAVILY_API_KEY')
+GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY') 
