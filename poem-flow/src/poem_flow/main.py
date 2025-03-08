26fabd4745ca4d3667b38ac656279a53b9de8b85
Sufyan Ahmed <muhammadsufyan20@gmail.com>
2025-03-08 17:24:09 +0500

    poem-flow

--- poem-flow/src/poem_flow/main.py
+++ poem-flow/src/poem_flow/main.py
@@ -1,25 +1,46 @@
 from poem_flow.crews.poem_crew.poem_crew import PoemCrew
 from crewai.flow.flow import Flow, listen, start
-from litellm import completion
 from dotenv import load_dotenv, find_dotenv
+from random import randint 
+from pydantic import BaseModel, Field
 import os
 
 _:bool = load_dotenv(find_dotenv())
 model = os.getenv("MODEL")
 
+class PoemConfig(BaseModel):
+    sentence_count: int = 1
+    poem: str = ""
+    
 # this is the main flow of the entire CrewAI project
-class PeomFlow(Flow):
+class PoemFlow(Flow[PoemConfig]):
     @start()
+    def gen_sentence_count(self):
+        self.state.sentence_count = randint(1, 4)     
+    
+    @listen(gen_sentence_count)
     def generate_poem(self):
         result = PoemCrew().poem_crew().kickoff(
             inputs = {
-                'topic': "generate a poem on the topic of LangGraph" 
-            }
-        )
-        print(result.raw)
-        return result
+                "sentence_count": self.state.sentence_count,
+                "topic"         : "CrewAI"
+                })
+        self.state.poem = result.raw
+    
+    @listen(generate_poem)
+    def save_poem(self):
+        return {
+            "poem": self.state.poem,
+            "author": "Sufyan"
+        }
+    
 
 def kickoff():
-    flow = PeomFlow()
+    flow = PoemFlow()
     response = flow.kickoff()
     print(f" Generated Topic:{response}")
+    
+    
+def plot():
+    poem_flow = PoemFlow()
+    poem_flow.plot()
